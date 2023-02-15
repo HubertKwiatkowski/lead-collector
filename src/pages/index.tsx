8c7a150177598855de6868ec4f1f6fc66f2ad03f
Hubert <h.kwiatkowski@hotmail.com>
2023-02-15 13:00:52 +0100

    fixed some things

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,33 +6,58 @@ import { Button, Checkbox, Link, Switch, TextField } from "nerdux-ui-system";
 import * as styles from "./index.module.scss";
 
 const IndexPage: React.FC<PageProps> = () => {
+  const switchHandle = () => {};
+
+  const textboxHandle = () => {};
+
+  const checkboxHandle = () => {};
+
+  const buttonHandle = () => {};
+
   return (
     <main>
       <Container>
+        <aside></aside>
+
         <header className={styles.titleHeader}>
           <h1>
             Join the Gameboy <br />
             <span>waiting list</span>
           </h1>
+        </header>
+        <section>
           <div>
             <p>I swear, I’m a classic gameboy fan</p>
-            <Switch></Switch>
+            <Switch onChange={switchHandle} id={"switch"}></Switch>
           </div>
 
           <form action="">
             <TextField
+              id={"name"}
+              name={"name"}
+              onChange={textboxHandle}
+              value={""}
               type={"text"}
               label={"Name"}
               placeholder={"e.g. Richard Parker"}
             ></TextField>
             <TextField
+              id={"email"}
+              name={"email"}
+              onChange={textboxHandle}
+              value={""}
               type={"text"}
               label={"Email"}
               placeholder={"e.g. richard@gmail.com"}
             ></TextField>
             <div>
-              <Checkbox></Checkbox>
-              <p>I have read and accept the a</p>
+              <Checkbox
+                id={"checbox"}
+                name={"checbox"}
+                checked={false}
+                onChange={checkboxHandle}
+              ></Checkbox>
+              <p>I have read and accept the</p>
               <Link
                 to={
                   "https://nerdux.nerdbord.io/?path=/story/inputs-button--button"
@@ -42,9 +67,11 @@ const IndexPage: React.FC<PageProps> = () => {
                 privacy policy
               </Link>
             </div>
-            <Button>Sign me up!</Button>
+            <Button onClick={buttonHandle}>Sign me up!</Button>
           </form>
-        </header>
+        </section>
+
+        <aside></aside>
       </Container>
     </main>
   );
